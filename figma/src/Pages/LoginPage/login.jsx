dfd49c99d0cf6bd404d7267d71649644f49fd4a8
nazile <tu86djvrl@code.edu.az>
2023-12-25 00:34:39 +0400

    End front-end

--- figma/src/Pages/LoginPage/login.jsx
+++ figma/src/Pages/LoginPage/login.jsx
@@ -1,39 +1,33 @@
 import React from 'react'
-import './signup.scss'
-// import GoogleIcon from '../../images/GoogelIcon.png'
+import './login.scss'
 import { Link } from "react-router-dom";
 
-function SignUpPage() {
+function LogIn() {
   return (
-    <section id='signUpPage'>
-      <div className="signUpLeftBox">
+    <section id='logIn'>
+    <div className="signUpLeftBox">
         <img src="https://s3-alpha-sig.figma.com/img/75f3/94c0/a1c7dc5b68a42239311e510f54d8cd59?Expires=1703462400&Signature=nKEwCyRvEmZm6HugUvka3eT5IFLfeK8SyfvoInRtqA9ATUWRc-yt7P0fH75hfO-xPHOeOtuH72PmYjB4FkSxPIbYxDbsGA6ShmFfmhqSPUuiBFpzHS7bYKzUkP3tw5GUMOgYnDCKnw8PMKtJAWhcdnsQYjr3T1JHZ0ZUVsseQeCdFzIM7xVfjfnAEQ9t8nFILMB6~ftbsDW0d-lp-V1qdKtyPFmtYA2ykoIdXEDTJlVTmAY-O6F2csHFS81CHf9IPToNfLx0zsdHu8hsXqdBmnAQ334~tTrwmnJfUt8jLghL3gORo4O7V0IOxp7Yb~rgUfYIfJzwPmzYCY3abxN5jg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
       </div>
       <div className="signUpRightBox">
         <div className="logUpBox">
-          <h1>Create an account</h1>
+          <h1>Log in to Exclusive</h1>
           <p>Enter your details below</p>
           <form action="" style={{ display: "flex", flexDirection: 'column' }}>
-            <input type="text" placeholder='Name' />
             <input type="email" placeholder='Email or Phone Number' />
-            <input type="password" placeholder='Password' />
+            <input type="text" placeholder='Password' />
           </form>
-          <div className="crateAccountBtn" style={{cursor:"pointer"}}>
-          Create Account
+         <div className="logInAndForgotBox">
+         <div className="logInBox">
+          <Link style={{textDecoration:"none", color:"white"}}>Log In</Link>
           </div>
-          <div className="signUpGoogle">
-            {/* <img src={GoogleIcon} alt="" /> */}
-            <Link style={{textDecoration:"none", color:"black"}}>Sign up with Google</Link>
+          <div className="forgetBox">
+                <Link style={{textDecoration:"none", color:"#DB4444"}}>Forget Password?</Link>
           </div>
-          <div className="logInBox">
-            Already have account?
-            <Link to={"/logIn"}><span>Log in</span></Link>
-          </div>
+         </div>
         </div>
       </div>
-
     </section>
   )
 }
 
-export default SignUpPage
+export default LogIn
